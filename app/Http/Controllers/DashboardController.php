60200c5ecffa6c76e0500e512db73fdef7dc785d
Biruk Lemma <biruklemmadebela@gmail.com>
2024-04-07 12:30:32 +0300

    remove a comment

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,15 +10,6 @@ class DashboardController extends Controller
     //
     public function index() 
     {
-        // if (Auth::user()->hasRole('student')) {
-        //     return view('dashboard.student');
-        // } elseif (Auth::user()->hasRole('health_professional')) {
-
-        //     return view('dashboard.health_professional');
-        // } elseif (Auth::user()->hasRole('admin')) {
-
-        //     return view('dashboard.admin');
-        // }
 
         if(Auth::user()->role_id === 'student'){
             return view('dashboard.student');
