583ae5de1e5b1b4e9f184adf2fd2e20375d47ed5
Biruk Lemma <biruklemmadebela@gmail.com>
2024-04-08 10:26:54 +0300

    drug, alchol and depression tracking

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -22,23 +22,38 @@
                 </div>
             </div>
         @endif
-            <div class="p-4 sm:p-8 bg-white  shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-picture')
-                </div>
-            </div>
 
-            <div class="p-4 sm:p-8 bg-white  shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
+            <section class="p-3 rounded-xl bg-custom-graish mt-10 flex justify-start h-auto">
+                <div class="w-custom-4  rounded-xl overflow-hidden">
+                <img src="{{asset('images/helena-lopes.jpg')}}" alt="">
                 </div>
-            </div>
+                <div class="flex flex-col py-20 px-28">
+                <p class="font-semibold">
+                    what is your main concern right now?
+                </p>
 
-            <div class="p-4 sm:p-8 bg-white  shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
+                <div class="flex flex-col"> 
+                    <a href="/">
+                        <button class="bg-custom-blue hover:bg-transparent hover:text-black hover:outline hover:outline-1 transition-all duration-200 ease-in-out text-white w-40 p-3 text-sm rounded-full mt-5"><span>Depression</span> &gt;</button>
+                    </a>
+
+                    <button class="bg-custom-blue hover:bg-transparent hover:text-black hover:outline hover:outline-1 transition-all duration-200 ease-in-out text-white w-40 p-3 text-sm rounded-full mt-5"><span>Alcholism</span> &gt;</button>
+
+                    <button class="bg-custom-blue hover:bg-transparent hover:text-black hover:outline hover:outline-1 transition-all duration-200 ease-in-out text-white w-40 p-3 text-sm rounded-full mt-5"><span>Drug</span> &gt;</button>
                 </div>
-            </div>
+  
+                </div>
+            </section>
+
+            <section class="p-3 rounded-xl bg-custom-graish mt-10 flex justify-start h-auto">
+                <div class="w-custom-4  rounded-xl overflow-hidden">
+                <img src="{{asset('images/zachary-nelson.jpg')}}" alt="">
+                </div>
+                <div class="flex flex-col py-20 px-28">
+                <p class="font-semibold">Learn university life psychology now and teach your friend always to be happy!</p>
+                <button class="bg-black text-white w-40 p-3 text-sm rounded-full mt-5"><span>Learn More</span> &gt;</button>
+                </div>
+            </section>
         </div>
     </div>
 
